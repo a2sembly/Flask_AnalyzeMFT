2094dd288559696ee34e1d03d3b5f9690e46a6e6
a2sembly <lsh970804@gmail.com>
2021-04-07 22:21:39 +0900

    Bug Fix
    2021-04-07

--- etc.py
+++ etc.py
@@ -3,7 +3,33 @@ import os
 import subprocess
 from subprocess import Popen, PIPE
 from subprocess import check_output
+import pandas as pd
 root_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def Checkfile(file_):
+    csv_ = file_ + '.csv'
+    html = file_ + '.html'
+    file = os.path.isfile(csv_)
+    html_ = os.path.isfile(html)
+    print(html_)
+    print(csv_)
+    if file == True:
+        if html == True:
+            print(os.path.getsize(csv_))
+            if os.path.getsize(csv_) > 10 :
+                csv_to_html(csv_)
+                return 'progress'
+        else:
+            csv_to_html(csv_)
+            return 'inprogress'
+    else:
+        return 'Fail'
+
+def csv_to_html(file_CSV):
+    df = pd.read_csv(root_dir + '\\' + file_CSV)
+    r_name = os.path.basename(file_CSV)
+    df.to_html(r_name.replace('.csv','.html'))
 
 def calc_file_hash(data):
     hash = hashlib.md5(data).hexdigest()
@@ -17,8 +43,8 @@ def createFolder(directory):
         print ('Error: Creating directory. ' +  directory)
 
 def get_shell_script_output_using_communicate(filename):
-    file_ = os.path.basename(filename) #파일명만 추출
-    print(filename)
-    subp_arg = root_dir + '\\test.bat ' + filename + ' ' + file_
+    # file_ = os.path.basename(filename) #파일명만 추출
+    # print(filename)
+    subp_arg = root_dir + '\\test.bat ' + filename + ' ' + filename
     print(subp_arg)
     subprocess.call(subp_arg)
--- flask_upload.py
+++ flask_upload.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, make_response, jsonify, Flask
+from flask import render_template, request, make_response, jsonify, Flask, abort, redirect
 from werkzeug.utils import secure_filename
 import os
 import hashlib
@@ -7,37 +7,60 @@ import time
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-    
-#C:\Users\rever\AppData\Local\Programs\Python\Python37\python.exe
-#파일 업로드 처리
-@app.route('/fileUpload', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      #저장할 경로 + 파일명
-      f.save(secure_filename(f.filename))
-      return 'uploads 디렉토리 -> 파일 업로드 성공!'
+app.config['full_filepath'] = ''
 
 @app.route("/upload", methods=["GET", "POST"])
-def upload_video():
+def upload():
     if request.method == "POST":
-
-        file = request.files["file"]
-        print("File uploaded")
-        res = make_response(jsonify({"message": "File uploaded"}), 200)
-        hash_val = calc_file_hash(file.read())
-        createFolder(os.path.join(app.config['UPLOAD_FOLDER'], hash_val))
-        full_dirpath = os.path.join(app.config['UPLOAD_FOLDER'], hash_val)
-        print(full_dirpath)
-        time.sleep(1)
-        full_filepath = full_dirpath + '\\' + secure_filename(hash_val)
-        file.seek(0) # hash 계산으로 file offest 위치가 마지막이므로 처음으로 옮김
-        file.save(full_filepath)
-        print(full_filepath)
-        get_shell_script_output_using_communicate(full_filepath)
-        return res
+        if request.form['type'] == 'Upload_file':
+            file = request.files["file"]
+            print("File uploaded")
+            res = make_response(jsonify({"message": "File uploaded"}), 200)
+            hash_val = calc_file_hash(file.read())
+            createFolder(os.path.join(app.config['UPLOAD_FOLDER'], hash_val))
+            full_dirpath = os.path.join(app.config['UPLOAD_FOLDER'], hash_val)
+            print(full_dirpath)
+            time.sleep(1)
+            full_filepath = full_dirpath + '\\' + secure_filename(hash_val)
+            file.seek(0) # hash 계산으로 file offest 위치가 마지막이므로 처음으로 옮김
+            file.save(full_filepath)
+            app.config['full_filepath'] = full_filepath
+            print(app.config['full_filepath'])
+            return res
+        elif request.form['type'] == 'Analyze_file':
+            print("analyze")
+            analyze()
+            
 
     return render_template("/test.html")
+    
+def analyze():
+        print(app.config['full_filepath'])
+        while(1):
+            val = Checkfile(app.config['full_filepath'])
+            if val == 'progress':
+               return render_template("/" + app.config['full_filepath'] + ".html")
+            elif val == 'inprogress':
+               continue
+            else:
+               get_shell_script_output_using_communicate(app.config['full_filepath'])
+
+
+@app.route("/Loading", methods=["GET", "POST"])
+def load():
+    return render_template("/Loading.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('/404.html'), 404
+
+@app.errorhandler(401)
+def error_401(error):
+    return render_template('/401.html'), 401
+
+@app.errorhandler(405)
+def error_405(error):
+    return render_template('/405.html'), 405
 
 if __name__ == '__main__':
     #서버 실행
